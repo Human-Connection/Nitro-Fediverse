9fd58dba2b8bfb2424f73ebd9fe46c5ab3fe7d51
Armin <armin@kunkel24.de>
2019-02-28 01:38:47 +0100

    Split utils index file into different files

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,181 +1,100 @@
-import crypto from 'crypto'
-import { signAndSend } from './signing'
-import as from 'activitystrea.ms'
-import ap from '../index'
+import { activityPub } from '../ActivityPub'
+import gql from 'graphql-tag'
+import { createSignature } from '../security'
+import request from 'request'
 const debug = require('debug')('ea:utils')
 
-export function extractNameFromId(url) {
-    const urlObject = new URL(url)
-    const pathname = urlObject.pathname
-    const splitted = pathname.split('/')
+export function extractNameFromId (uri) {
+  const urlObject = new URL(uri)
+  const pathname = urlObject.pathname
+  const splitted = pathname.split('/')
 
-    return splitted[splitted.indexOf('users') + 1]
+  return splitted[splitted.indexOf('users') + 1]
 }
 
-export function constructIdFromName(name) {
-    return `https://${ap.domain}/users/${name}`
+export function extractIdFromActivityId (uri) {
+  const urlObject = new URL(uri)
+  const pathname = urlObject.pathname
+  const splitted = pathname.split('/')
+
+  return splitted[splitted.indexOf('status') + 1]
 }
 
-export function extractDomainFromUrl(url) {
-    return new URL(url).hostname;
+export function constructIdFromName (name, fromDomain = activityPub.domain) {
+  return `http://${fromDomain}/activitypub/users/${name}`
 }
 
-export function sendCollection(collectionName, req, res) {
-  const name = req.params.name;
-  const id = constructIdFromName(name)
+export function extractDomainFromUrl (url) {
+  return new URL(url).hostname
+}
 
-  switch (collectionName) {
-    case 'followers':
-      attachThenCatch(req.app.get('ap').getFollowersCollection(id), res)
-      break
-
-    case 'followersPage':
-      attachThenCatch(req.app.get('ap').getFollowersCollectionPage(id), res)
-      break
-
-    case 'following':
-      attachThenCatch(req.app.get('ap').getFollowingCollection(id), res)
-      break
-
-    case 'followingPage':
-      attachThenCatch(req.app.get('ap').getFollowingCollectionPage(id), res)
-      break
-
-    case 'outbox':
-      attachThenCatch(req.app.get('ap').getOutboxCollection(id), res)
-      break
-
-    case 'outboxPage':
-      attachThenCatch(req.app.get('ap').getOutboxCollectionPage(id), res)
-      break
-
-    default:
-      res.status(500).end();
+export function throwErrorIfApolloErrorOccurred (result) {
+  if (result.error && (result.error.message || result.error.errors)) {
+    throw new Error(`${result.error.message ? result.error.message : result.error.errors[0].message}`)
   }
 }
 
-function attachThenCatch(promise, res) {
-  return promise
-    .then((collection)=>{
-      res.status(200).contentType('application/activity+json').send(collection)
+export function signAndSend (activity, fromName, targetDomain, url) {
+  // fix for development: replace with http
+  url = url.indexOf('localhost') > -1 ? url.replace('https', 'http') : url
+  debug(`passhprase = ${process.env.PRIVATE_KEY_PASSPHRASE}`)
+  return new Promise(async (resolve, reject) => {
+    debug('inside signAndSend')
+    // get the private key
+    const result = await activityPub.dataSource.client.query({
+      query: gql`
+          query {
+              User(slug: "${fromName}") {
+                  privateKey
+              }
+          }
+      `
     })
-    .catch(()=>{
-      debug(`error getting a Collection`)
-      res.status(500).end();
-    })
-}
 
-export function createActor(name, pubkey) {
-    return {
-        '@context': [
-            'https://www.w3.org/ns/activitystreams',
-            'https://w3id.org/security/v1'
-        ],
-        'id': `https://${ap.domain}/users/${name}`,
-        'type': 'Person',
-        'preferredUsername': `${name}`,
-        'name': `${name}`,
-        'following': `https://${ap.domain}/users/${name}/following`,
-        'followers': `https://${ap.domain}/users/${name}/followers`,
-        'inbox': `https://${ap.domain}/users/${name}/inbox`,
-        'outbox': `https://${ap.domain}/users/${name}/outbox`,
-        'url': `https://${ap.domain}/@${name}`,
-        'endpoints': {
-            'sharedInbox': `https://${ap.domain}/inbox`
-        },
-        'publicKey': {
-            'id': `https://${ap.domain}/users/${name}#main-key`,
-            'owner': `https://${ap.domain}/users/${name}`,
-            'publicKeyPem': pubkey
-        }
-    }
-}
+    if (result.error) {
+      reject(result.error)
+    } else {
+      // add security context
+      const parsedActivity = JSON.parse(activity)
+      if (Array.isArray(parsedActivity['@context'])) {
+        parsedActivity['@context'].push('https://w3id.org/security/v1')
+      } else {
+        const context = [parsedActivity['@context']]
+        context.push('https://w3id.org/security/v1')
+        parsedActivity['@context'] = context
+      }
 
-export function createWebFinger(name) {
-    return {
-        'subject': `acct:${name}@${ap.domain}`,
-        'links': [
+      // deduplicate context strings
+      parsedActivity['@context'] = [...new Set(parsedActivity['@context'])]
+      const privateKey = result.data.User[0].privateKey
+      const date = new Date().toUTCString()
+
+      debug(`url = ${url}`)
+      request({
+        url: url,
+        headers: {
+          'Host': targetDomain,
+          'Date': date,
+          'Signature': createSignature(privateKey, `http://${activityPub.domain}/activitypub/users/${fromName}#main-key`, url,
             {
-                'rel': 'self',
-                'type': 'application/activity+json',
-                'href': `https://${ap.domain}/users/${name}`
-            }
-        ]
-    }
-}
-
-export function createOrderedCollection(name, collectionName) {
-    return {
-        "@context": "https://www.w3.org/ns/activitystreams",
-        "id": `https://${ap.domain}/users/${name}/${collectionName}`,
-        "summary": `${name}s ${collectionName} collection`,
-        "type": "OrderedCollection",
-        "first": `https://${ap.domain}/users/${name}/${collectionName}?page=true`,
-        "totalItems": 0
-    }
-}
-
-export function createOrderedCollectionPage(name, collectionName) {
-    return {
-        "@context": "https://www.w3.org/ns/activitystreams",
-        "id": `https://${ap.domain}/users/${name}/${collectionName}?page=true`,
-        "summary": `${name}s ${collectionName} collection`,
-        "type": "OrderedCollectionPage",
-        "totalItems": 0,
-        "partOf": `https://${ap.domain}/users/${name}/${collectionName}`,
-        "orderedItems": []
-    }
-}
-
-export function createNoteActivity(text, name) {
-    const createUuid = crypto.randomBytes(16).toString('hex')
-    const noteUuid = crypto.randomBytes(16).toString('hex')
-    let d = new Date()
-
-    return {
-        '@context': 'https://www.w3.org/ns/activitystreams',
-
-        'id': `https://${ap.domain}/users/${name}/status/${createUuid}`,
-        'type': 'Create',
-        'actor': `https://${ap.domain}/users/${name}`,
-
-        'object': {
-            'id': `https://${ap.domain}/users/${name}/status/${noteUuid}`,
-            'type': 'Note',
-            'published': d.toISOString(),
-            'attributedTo': `https://${ap.domain}/users/${name}`,
-            'content': text,
-            'to': 'https://www.w3.org/ns/activitystreams#Public'
+              'Host': targetDomain,
+              'Date': date,
+              'Content-Type': 'application/activity+json'
+            }),
+          'Content-Type': 'application/activity+json'
+        },
+        method: 'POST',
+        body: JSON.stringify(parsedActivity)
+      }, (error, response) => {
+        if (error) {
+          debug(`Error = ${JSON.stringify(error, null, 2)}`)
+          reject(error)
+        } else {
+          debug('Response Headers:', JSON.stringify(response.headers, null, 2))
+          debug('Response Body:', JSON.stringify(response.body, null, 2))
+          resolve()
         }
+      })
     }
-}
-
-export function sendAcceptActivity(theBody, name, targetDomain, url) {
-    as.accept()
-        .id(`https://${ap.domain}/users/${name}/status/` + crypto.randomBytes(16).toString('hex'))
-        .actor(`https://${ap.domain}/users/${name}`)
-        .object(theBody)
-        .prettyWrite((err, doc) => {
-            if (!err) {
-                return signAndSend(doc, name, targetDomain, url)
-            } else {
-                debug(`error serializing Accept object: ${err}`)
-                throw new Error(`error serializing Accept object`)
-            }
-        })
-}
-
-export function sendRejectActivity(theBody, name, targetDomain, url) {
-    as.reject()
-        .id(`https://${ap.domain}/users/${name}/status/` + crypto.randomBytes(16).toString('hex'))
-        .actor(`https://${ap.domain}/users/${name}`)
-        .object(theBody)
-        .prettyWrite((err, doc) => {
-            if (!err) {
-                return signAndSend(doc, name, targetDomain, url)
-            } else {
-                debug(`error serializing Accept object: ${err}`)
-                throw new Error(`error serializing Accept object`)
-            }
-        })
+  })
 }
